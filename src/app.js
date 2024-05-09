cc123fe728b148abb93749093934cd59ee7c63ff
titonatos <titanium4ik777@gmail.com>
2024-05-09 11:51:23 +0300

    update app.js. Step 5 done

--- src/app.js
+++ src/app.js
@@ -2,50 +2,90 @@ import onChange from 'on-change';
 import i18n from 'i18next';
 import resources from './locales/index.js';
 import * as yup from 'yup';
-import { setLocale } from 'yup';
 import axios from 'axios';
 
-const schema = yup.string().trim().required().url().matches(/^https:\/\/.*feed$/);
+axios.defaults.cache = false;
+
+const validation = (url, links, i18Instance) => {
+  const schema = yup
+    .string()
+    .trim()
+    .required()
+    .url(i18Instance.t('invalidURLMsg'))
+    .notOneOf(links, i18Instance.t('duplicateErrorMsg'))
+    .validate(url);
+  return schema;
+};
 
 const render = (state) => {
   if (!state.form.isValide) {
     state.elementsUI.errorMsg.textContent = state.form.error;
     state.elementsUI.input.classList.add('isInvalid');
-
+    
   } else {
     state.elementsUI.input.classList.remove('isInvalid');
     state.elementsUI.errorMsg.textContent = '';
-
+    
     state.elementsUI.form.reset();
   }
   
-  if (!state.feeds.length) {
-    const news = [];
+  if (state.posts.length > 0) {
     const ul = document.createElement('ul');
     const container = document.querySelector('.feeds-container');
     
-    state.feeds.forEach(feed => {
+    state.posts.forEach(post => {
       const li = document.createElement('li');
       const a = document.createElement('a');
-      a.textContent = feed.title;
-      a.href = feed.link;
-      li.prepend(a);
-      news.push(li);
+      a.textContent = post.title;
+      a.href = post.link;
+      li.append(a);
+      ul.append(li);
     });
-
-    ul.prepend(news.join(''));
-    container.prepend(ul);
+    
+    container.innerHTML = '';
+    container.append(ul);
   }
 };
 
 const init = (instance, state) => {
-  setLocale({
-    mixed: {
-      default: instance.t('invalidURLMsg'),
-    },
-  });
-
   state.elementsUI.button.textContent = instance.t('buttonText');
+};
+
+const normolizePosts = (parsedPosts, feedID) => {
+  const posts = [];
+  
+  parsedPosts.forEach(item => {
+    const title = item.querySelector('title').textContent;
+    const description = item.querySelector('description').textContent;
+    const link = item.querySelector('link').textContent;
+    
+    posts.push({ feedID, title, description, link });
+  });
+  
+  return posts;
+};
+
+const addProxy = (url) => {
+  const proxyUrl = new URL('/get', 'https://allorigins.hexlet.app');
+  proxyUrl.searchParams.append('disableCache', 'true');
+  proxyUrl.searchParams.append('url', url);
+  return proxyUrl.toString();
+};
+
+const makeRequest = (link) => {
+  console.log(addProxy(link));
+  return axios.get(addProxy(link))
+    .catch(err => {
+      throw Error('invalidResourceMsg')
+    });
+};
+
+const getFeedPosts = (state, response) => {
+  const responseContent = response.data.contents;
+  const parser = new DOMParser();
+  const parsedPosts = parser.parseFromString(responseContent, 'text/xml').querySelectorAll('item');
+  const feedID = state.links.indexOf(response.data.status.url);
+  return normolizePosts(parsedPosts, feedID);
 };
 
 export default () => {
@@ -65,56 +105,54 @@ export default () => {
       errorMsg: document.querySelector('p.errorMsg'),
     },
     feeds: [],
+    links: [],
+    posts: [],
   };
 
-  const watcher = onChange(state, ((path, value, preiousValue) => {
+  const watcher = onChange(state, ((path, value) => {
     if (path === 'form.input.value') {
-      console.log('if path');
-      Promise.resolve(state.form.isValide = schema.validate(value))
-        .then(() => {
-          state.form.error = (state.form.isValide) ? '' : i18nInstance.t('duplicateErrorMsg');
+      Promise.resolve(() => {
+        validation(value, state.feeds.map(feed => feed.link), i18nInstance);
+      })
+      .then(() => {
+        return makeRequest(value);
+      })
+      .then((response) => {
+        state.form.isValide = true;
+        state.links.push(value);
+        const normolizedPosts = getFeedPosts(watcher, response);
 
-          if (state.form.isValide) {
-            axios.get(`https://allorigins.hexlet.app/get?charset=UTF-8&url=${value}`)
-              .then((response) => {
-                const responseContent = response.data.contents;
-                const parser = new DOMParser();
-                const parsedItems = parser.parseFromString(responseContent, 'text/xml').querySelectorAll('item');
-                const newFeed = {
-                  link: value,
-                  news: [],
-                };
-                
-                parsedItems.forEach(item => {
-                  const title = item.querySelector('title').textContent;
-                  const description = item.querySelector('description').textContent;
-                  const link = item.querySelector('link').textContent;
-  
-                  const newsData = { title, description, link };
-                  newFeed.news.push(newsData);
-                });
-
-                state.feeds.push(newFeed);
-
-                console.log(state.feeds);
-              })
-              .then()
-              .catch((response) => {
-                console.log(`Error:\n${response}`);
-              });
-          }
-        })
-        .catch((err) => {
-          state.form.error = i18nInstance.t('invalidResourceMsg');
-          state.form.isValide = false;
-        })
-        .finally(() => {
+        state.posts = state.posts.concat(normolizedPosts);
+      }).catch((err) => {
+        state.form.error = i18nInstance.t(err.message);
+        state.form.isValide = false;
+      })
+      .finally(() => {
           render(state);
           state.form.error = '';
           state.form.input.value = '';
-        });
+      })
+      ;
     }
   }));
+
+  const updating = () => {
+    if (state.links.length > 0) {
+      Promise.all(state.links.map(link => makeRequest(link)))
+        .then((responses) => {
+          responses.forEach((response) => {
+            const oldLinks = state.posts.map(post => post.link);
+            const normolizedPosts = getFeedPosts(watcher, response);
+            const newPosts = normolizedPosts.filter(post => !oldLinks.includes(post.link));
+            state.posts = [...newPosts, ...state.posts];
+          });
+          render(state);
+        })
+        .catch((err) => {
+          console.log(`updating: ${i18nInstance.t(err.message)}`);
+        });
+    }
+  };
 
   const i18nInstance = i18n.createInstance();
 
@@ -136,5 +174,9 @@ export default () => {
     }
     
     form.addEventListener('submit', handler);
+    
+    
   });
+
+  setInterval(updating, 5000);
 };
